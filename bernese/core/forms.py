67baa8e77958fcc1b6ba1c72ebd604ea2dc6501c
Gabriel Diniz de Oliveira <engenheirogabrieloliveira@gmail.com>
2017-10-14 09:50:37 -0300

    add save msg backup
    Salva a mensagem enviada em arquivo de texto no servidor

--- bernese/core/forms.py
+++ bernese/core/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
-import threading
-
+from bernese.settings import BASE_DIR
 from bernese.core.mail import send_mail_template
+from datetime import datetime
+import threading
 
 class ContactGeneral(forms.Form):
 
@@ -33,6 +34,15 @@ class ContactGeneral(forms.Form):
 		}
 		template_name = 'contact_email.html'
 
+		# Salvando backup da mensagem no servidor
+		msg_txt = 'Em: ' + datetime.now().isoformat(' ','seconds') + '\n'
+		msg_txt += 'Nome: {name}\nE-mail: {email}\nMessage: {message}\n'.format(**context)
+
+		with open(BASE_DIR + '/backupMsgContato.txt','a') as f:
+		    print(msg_txt, file=f)
+
+
+		# Enviando o email em um processamento paralelo
 		threading.Thread(target=send_mail_template,
 			args = (subject, template_name, context, [settings.CONTACT_EMAIL])
 			).start()
